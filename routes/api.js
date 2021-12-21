40cb32839de39c54ffa5efff217cfd8d6af1a551
otto281 <otto_281@hotmail.com>
2021-12-22 10:45:02 +1100

    Final Stage 1

--- routes/api.js
+++ routes/api.js
@@ -7,9 +7,9 @@ router.get("/", api.ourFunction);
 router.get("/something", api.ourFunction2);
 
 // Not shown in stage 1 tutorial
-// Handle post requests to the root of the application
+// Handle post requests to the root of the route
 router.post("/", api.ourFunction);
-// Handle put requests to the root of the application
+// Handle put requests to the root of the route
 router.put("/", api.ourFunction);
-
+// Handle delete requests to the root of the route
 module.exports = router;
