9de8a60b296869896e1085cfbdc779ec7fa94044
otto281 <otto_281@hotmail.com>
2021-12-22 10:37:33 +1100

    Final Stage 1

--- public/index.js
+++ public/index.js
@@ -1,10 +1,21 @@
 const ourBtn = document.getElementById("ourFetched");
 const fetchResponse = document.getElementById("fetchedResponse");
+const selectIcon = document.querySelectorAll(".material-icons");
 
 function youClicked() {
   fetch("http://localhost:3000/api/something")
     .then((response) => response.text())
-    .then((info) => (fetchResponse.textContent = info));
+    .then((info) => (fetchResponse.textContent = info))
+    .catch((err) => alert(err));
 }
 
-ourBtn.addEventListener("click", youClicked);
+ourBtn.addEventListener("click", console.log(selectIcon));
+
+// Add event listener to all select icons
+for (x = 0; x < selectIcon.length; x++) {
+  selectIcon[x].addEventListener("click", (e) => {
+    let user =
+      e.target.parentElement.parentElement.textContent.split("send")[0];
+    console.log(user);
+  });
+}
--- routes/api.js
+++ routes/api.js
@@ -6,4 +6,10 @@ router.get("/", api.ourFunction);
 
 router.get("/something", api.ourFunction2);
 
+// Not shown in stage 1 tutorial
+// Handle post requests to the root of the application
+router.post("/", api.ourFunction);
+// Handle put requests to the root of the application
+router.put("/", api.ourFunction);
+
 module.exports = router;
